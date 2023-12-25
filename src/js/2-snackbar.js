95db312b6f6b8af68e69e0d43c64b2cf98a81ecb
Anastassia2312 <23122000na@gmail.com>
2023-12-25 18:25:41 +0200

    snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,41 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const submitForm = document.querySelector('.form');
+
+submitForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const delayInput = document.querySelector('input[name = "delay"]');
+  const delay = delayInput.value;
+  const state = document.querySelector('input[name="state"]:checked').value;
+
+  const message = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  message
+    .then(delay => {
+      console.log(`✅ Fulfilled promise in ${delay}ms`);
+      iziToast.success({
+        title: 'OK',
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      console.log(`❌ Rejected promise in ${delay}ms`);
+      iziToast.error({
+        title: 'Error',
+        message: 'Illegal operation',
+        position: 'topRight',
+      });
+    });
+});
